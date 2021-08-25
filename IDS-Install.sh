39e62b9fb978e967be5f178ead5c36bbda086242
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-08-24 20:33:18 -0400

    Update IDS-Install.sh

--- IDS-Install.sh
+++ IDS-Install.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
 # Update
+clear
+echo "Updating...
+echo
+sleep 5
 apt update && apt upgrade -y
 
 # Install Suricata
+clear
+echo "Installing Suricata..."
+echo
+sleep 5
 add-apt-repository ppa:oisf/suricata-stable
 apt update
 apt-get install suricata -y
@@ -11,8 +21,8 @@ nano /etc/suricata/suricata.yaml # Edit Home Net & Interface // Add /var/lib/sur
 mkdir /root/suricata-pi/snort-rules
 tar -xvf /root/suricata-pi/snortrules-snapshot-2983.tar.gz -C /root/suricata-pi/snort-rules/
 tar -xvf /root/suricata-pi/community-rules.tar.gz
-mv suricata-pi/community-rules/community.rules /var/lib/suricata/rules/
-mv suricata-pi/snort-rules/rules/*.rules /var/lib/suricata/rules/
+mv /root/suricata-pi/community-rules/community.rules /var/lib/suricata/rules/
+mv /root/suricata-pi/snort-rules/rules/*.rules /var/lib/suricata/rules/
 
 # Update Suricata // Enable Sources
 suricata-update
@@ -26,7 +36,16 @@ suricata-update enable-source etnetera/aggressive
 suricata-update enable-source tgreen/hunting
 suricata-update
 
+
+# Install Additional Rules
+mv /root/suricata-pi/suricata /etc/default/suricata 
+service suricata start
+
 # Enable IP Forwarding
+clear
+echo "Configuring Interfaces..."
+echo
+sleep 5
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
@@ -37,19 +56,27 @@ ifconfig wlan0 promisc
 
 
 # Install // Enable DNS Server
+clear
+echo "Installing DNS Server..."
+echo
+sleep 5
 apt-get install bind9 -y
 nano /etc/bind/named.conf.options # Add Forwarder
 service bind9 restart
 
 
-# unzip and drop in comm rules and snapshot (snort)
-wget https://github.com/ben3636/suricata-pi/blob/main/suricata
-mv /root/suricata-pi/suricata /etc/default/suricata 
-service suricata start
-
-
 # Install & Run Evebox
+clear
+echo "Installing Evebox..."
+echo
+sleep 5
 apt install unzip -y
 wget https://evebox.org/files/release/latest/evebox-0.14.0-linux-arm64.zip
 unzip evebox-0.14.0-linux-arm64.zip 
+clear
+echo "Install Completed!"
+echo
+echo "Starting Evebox...(Web Interface Will Be Available on Port 5636"
+sleep 10
 cd evebox-0.14.0-linux-arm64/ && ./evebox server -v -D . --datastore sqlite --input /var/log/suricata/eve.json --host 0.0.0.0
+
